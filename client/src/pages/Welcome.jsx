0f838632ac8e1d2f30c60d16a7485c8b1a59b45f
ZTsquared <Zia.Tyebjee@Gmail.com>
2024-04-03 22:31:34 +0200

    get user list route for welcome page updated

--- client/src/pages/Welcome.jsx
+++ client/src/pages/Welcome.jsx
@@ -1,66 +1,81 @@
-import {useState, useEffect} from 'react';
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
-// when making a new user if error code is 403 have the user choose a new user name. 
-// this is a bit slapdash, in future there could be other reasons that you get a 403 
+// when making a new user if error code is 403 have the user choose a new user name.
+// this is a bit slapdash, in future there could be other reasons that you get a 403
 // (like if they submit an age that is not an integer)
 
-function Welcome({currentUser, setCurrentUser}) {
-  const [allUsers, SetAllUsers] = useState([])
-  const [userNames, setUserNames] = useState([])
-  const [selectedUserId, setSelectedUserId] = useState(currentUser?.id)
+function Welcome({ currentUser, setCurrentUser }) {
+  const [allUsers, SetAllUsers] = useState([]);
+  const [userNames, setUserNames] = useState([]);
+  const [selectedUserId, setSelectedUserId] = useState(currentUser?.id);
 
   // below: gets all the users or page reload:
-  useEffect(() => {getUsers()}, [])
+  useEffect(() => {
+    getUsers();
+  }, []);
   // below:  sets the list of usernames any time the allUsers list changes
-  useEffect(() => {setUserNames(allUsers.map(({userName}) => userName))}, [allUsers])
-  // below: updates/refreshes the currentUser information.  The if statement is there becasue otherwise the 
+  useEffect(() => {
+    setUserNames(allUsers.map(({ userName }) => userName));
+  }, [allUsers]);
+  // below: updates/refreshes the currentUser information.  The if statement is there becasue otherwise the
   // value binding between selectedUser and the user selector drop down was causing the user to be
-  // logged out whenever you navigated to the welcome page. 
-  useEffect(() => {if (!currentUser || (selectedUserId !== currentUser.id)) {
-    setCurrentUser(
-      allUsers
-      .filter(
-        (e) => e.id === +selectedUserId
-      )[0]);
-  }}, [selectedUserId])
+  // logged out whenever you navigated to the welcome page.
+  useEffect(() => {
+    if (!currentUser || selectedUserId !== currentUser.id) {
+      setCurrentUser(allUsers.filter((e) => e.id === +selectedUserId)[0]);
+    }
+  }, [selectedUserId]);
 
-
-
-  async function getUsers (){
+  async function getUsers() {
     try {
       const resultJSON = await fetch(`api/users/`);
       const users = await resultJSON.json();
-      SetAllUsers(users)
-    }catch (err){
-      console.log(err)
+      SetAllUsers(users);
+    } catch (err) {
+      console.log(err);
     }
   }
 
-  function handleUserChange (e) {
-    console.log("handleUserChange")
+  function handleUserChange(e) {
+    console.log("handleUserChange");
     // setSelectedUserId(1)
-    setSelectedUserId(e.target.value)
+    setSelectedUserId(e.target.value);
   }
 
-
-  return (    
-  <div>
-    <h3>Hey</h3>
-    <h1>{currentUser ? currentUser.userName : "Kiddo"}</h1> <br /><br />
-    {currentUser && <Link to="/quiz">Let's Play!</Link>}
-    <br /><br /><br />
-    <p>{currentUser ? "Change player" : "Who's playing?"}</p>
-    <select name="userSelect" onChange={handleUserChange}>
-      <option value=""></option>
-      {/* {userNames.map((userName, i) => <option key={i} value={userName}>{userName}</option>)} */}
-      {allUsers.map((user, i) => <option key={i} value={user.id}>{user.userName}</option>)} 
-    </select>
-    <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-    <Link to="/parentPortal">Parent Portal</Link>
-
-  </div>
-  )
+  return (
+    <div>
+      <h3>Hey</h3>
+      <h1>{currentUser ? currentUser.name : "Kiddo"}</h1> <br />
+      <br />
+      {currentUser && <Link to="/quiz">Let's Play!</Link>}
+      <br />
+      <br />
+      <br />
+      <p>{currentUser ? "Change player" : "Who's playing?"}</p>
+      <select name="userSelect" onChange={handleUserChange}>
+        <option value=""></option>
+        {/* {userNames.map((userName, i) => <option key={i} value={userName}>{userName}</option>)} */}
+        {allUsers.map((user, i) => (
+          <option key={i} value={user.id}>
+            {user.name}
+          </option>
+        ))}
+      </select>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <Link to="/parentPortal">Parent Portal</Link>
+    </div>
+  );
 }
 
-export default Welcome
+export default Welcome;
--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,23 @@
-var express = require('express');
-var router = express.Router();
-const db = require('../model/helper')
+const express = require("express");
+const router = express.Router();
+// const db = require('../model/helper');
+const models = require("../models");
+const { Op, Association } = require("sequelize");
 const mustExist = require("../guardFunctions/mustExist");
 const mustNotExist = require("../guardFunctions/mustNotExist");
 
 /* GET users listing. */
-router.get('/', async function(req, res, next) {
+router.get("/", async function (req, res, next) {
   try {
-    const resultsObject = await db("SELECT * FROM users;")
-    const users = resultsObject.data;
+    const users = await models.User.findAll({
+      attributes: ["name", "id"],
+    });
+    console.log("hi");
     res.send(users);
-  }catch(err) {
+  } catch (err) {
     res.status(500).send(err);
   }
 });
-
 
 // // get by user id
 // router.get('/:userName', mustExist("userName", "users", "userName"), async function(req, res, next) {
@@ -29,47 +32,64 @@ router.get('/', async function(req, res, next) {
 // });
 
 // this is an old version of the route where i get by userName
-router.get('/:userName', mustExist("userName", "users", "userName"), async function(req, res, next) {
-  console.log("getting a particular user")
-  try {
-    const {userName} = req.params;
-    const user = await db(`SELECT * FROM users WHERE userName= "${userName}";`)
-    res.send(user.data[0]);
-  }catch (err) {
-    res.status(500).send(err);
+router.get(
+  "/:userName",
+  mustExist("userName", "users", "userName"),
+  async function (req, res, next) {
+    console.log("getting a particular user");
+    try {
+      const { userName } = req.params;
+      const user = await db(
+        `SELECT * FROM users WHERE userName= "${userName}";`
+      );
+      res.send(user.data[0]);
+    } catch (err) {
+      res.status(500).send(err);
+    }
   }
-});
+);
 
+router.get(
+  "/:id/jokes",
+  mustExist("id", "users", "id"),
+  async function (req, res, next) {
+    try {
+      const { id } = req.params;
+      const jokes = await db(
+        `SELECT * FROM jokes WHERE jokes.id in (select joke_id from usersJokes where user_id = ${id});`
+      );
+      res.send(jokes.data);
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  }
+);
 
-router.get('/:id/jokes', mustExist("id", "users", "id"), async function(req, res, next) {
-try {
-  const {id} = req.params;
-  const jokes = await db(`SELECT * FROM jokes WHERE jokes.id in (select joke_id from usersJokes where user_id = ${id});`)
-  res.send(jokes.data);
-}catch (err) {
-  res.status(500).send(err);
-}
-});
-
-
-
-//post a new user.  userName should be unique so we have middleware to check this. the mustNotExist function builds a 
+//post a new user.  userName should be unique so we have middleware to check this. the mustNotExist function builds a
 // custom guard function based on the table and column name you want to check against
-router.post('/',  mustNotExist("userName", "users", "userName"), async function(req, res, next) {
-  console.log(!req.body.userName || !req.body.userName)
-  if (!req.body.userName || !req.body.userAge){
-    res.status(400).send({msg: "Submission does not contain a valid 'userName' and / or 'userAge' properties"})
+router.post(
+  "/",
+  mustNotExist("userName", "users", "userName"),
+  async function (req, res, next) {
+    console.log(!req.body.userName || !req.body.userName);
+    if (!req.body.userName || !req.body.userAge) {
+      res.status(400).send({
+        msg: "Submission does not contain a valid 'userName' and / or 'userAge' properties",
+      });
+    }
+    const { userName, userAge } = req.body;
+    try {
+      await db(
+        `INSERT INTO users (userName, userAge) values ("${userName}", ${userAge});`
+      );
+      res.send({
+        msg: `User '${userName}' with age '${userAge}' successfully added to database`,
+      });
+    } catch (err) {
+      res.status(500).send(err);
+    }
   }
-  const {userName, userAge} = req.body;
-  try {
-    await db(`INSERT INTO users (userName, userAge) values ("${userName}", ${userAge});`)
-    res.send({msg: `User '${userName}' with age '${userAge}' successfully added to database`});
-  } catch (err){
-    res.status(500).send(err)
-  }  
-});
-
-
+);
 
 // // add a joke to the user's personal library (via junction table usersJokes).  i have moved this to the usersjokes route.
 // router.post('/addToUserLibrary', async function(req, res, next) {
@@ -83,81 +103,93 @@ router.post('/',  mustNotExist("userName", "users", "userName"), async function(
 //     res.send({msg: `Joke_${joke_id} successfully added to user_${user_id}'s library`});
 //   } catch (err){
 //     res.status(500).send(err)
-//   }  
+//   }
 // });
 
-
-
-// later i would like to have: - household groupings of users, ability to add users and change a userName from the login page, ability to merge stars and jokes from one account into another if you delete the first, 
-
+// later i would like to have: - household groupings of users, ability to add users and change a userName from the login page, ability to merge stars and jokes from one account into another if you delete the first,
 
 // increase the user's balance by a quantity specified in the body of the request. quantity property must = a number.
 // in reality it might logical to combine this and the function below it into one, since any time you add to the balance you also add to the lifetime total.
-// but for now i wrote them individually. 
-router.put('/:id/increaseBalance/',  mustExist("id", "users", "id"), async function(req, res, next) {
-  console.log("put")
-  console.log(isNaN(+req.body.quantity))
-  if (!req.body.quantity || !req.params.id){
-    console.log("if")
-    res.status(422).send({msg: "Submission does not contain valid data"})
-  } else if (isNaN(+req.body.quantity)){
-    console.log("else if")
-    res.status(422).send({msg: "Amount of desired increase must be a number (data type number)"})
-  } else {
-    try {
-      console.log("else")
-      const {id} = req.params;
-      const {quantity} = req.body;
-      await db(`UPDATE users SET balance = balance+${quantity} WHERE id = "${id}";`)
-      res.send({msg: `User '${id}' balance increased by ${quantity}`});
-    } catch (err){
-      res.status(500).send(err)
-    }  
+// but for now i wrote them individually.
+router.put(
+  "/:id/increaseBalance/",
+  mustExist("id", "users", "id"),
+  async function (req, res, next) {
+    console.log("put");
+    console.log(isNaN(+req.body.quantity));
+    if (!req.body.quantity || !req.params.id) {
+      console.log("if");
+      res.status(422).send({ msg: "Submission does not contain valid data" });
+    } else if (isNaN(+req.body.quantity)) {
+      console.log("else if");
+      res.status(422).send({
+        msg: "Amount of desired increase must be a number (data type number)",
+      });
+    } else {
+      try {
+        console.log("else");
+        const { id } = req.params;
+        const { quantity } = req.body;
+        await db(
+          `UPDATE users SET balance = balance+${quantity} WHERE id = "${id}";`
+        );
+        res.send({ msg: `User '${id}' balance increased by ${quantity}` });
+      } catch (err) {
+        res.status(500).send(err);
+      }
+    }
   }
-});
-
-
-
+);
 
 //increase lifetime total, similar to increase balance
-router.put('/:id/increaseLifetimeTotal/',  mustExist("id", "users", "id"), async function(req, res, next) {
-  if (!req.body.quantity || !req.params.id){
-    res.status(422).send({msg: "Submission does not contain valid data"})
-  } else if (isNaN(+req.body.quantity)){
-    res.status(422).send({msg: "Amount of desired increase must be an number (data type number)"})
-  } else {
-    try {
-      const {id} = req.params;
-      const {quantity} = req.body;
-      await db(`UPDATE users SET lifetimeTotal = lifetimeTotal+${quantity} WHERE id = "${id}";`)
-      res.send({msg: `User '${id}' lifetimeTotal increased by ${quantity}`});
-    } catch (err){
-      res.status(500).send(err)
-    }  
+router.put(
+  "/:id/increaseLifetimeTotal/",
+  mustExist("id", "users", "id"),
+  async function (req, res, next) {
+    if (!req.body.quantity || !req.params.id) {
+      res.status(422).send({ msg: "Submission does not contain valid data" });
+    } else if (isNaN(+req.body.quantity)) {
+      res.status(422).send({
+        msg: "Amount of desired increase must be an number (data type number)",
+      });
+    } else {
+      try {
+        const { id } = req.params;
+        const { quantity } = req.body;
+        await db(
+          `UPDATE users SET lifetimeTotal = lifetimeTotal+${quantity} WHERE id = "${id}";`
+        );
+        res.send({
+          msg: `User '${id}' lifetimeTotal increased by ${quantity}`,
+        });
+      } catch (err) {
+        res.status(500).send(err);
+      }
+    }
   }
-});
-
-
-
+);
 
 // delete a user base on their id.
-router.delete('/:id', mustExist("id", "users", "id"), async function(req, res, next) {
-  try {
-    const {id} = req.params;
-    await db(`DELETE FROM users WHERE id="${id}";`)
-    res.send({msg: `User with id '${id}' successfully deleted from database`});
-  } catch (err){
-    console.log(err)
-    res.status(500).send(err)
+router.delete(
+  "/:id",
+  mustExist("id", "users", "id"),
+  async function (req, res, next) {
+    try {
+      const { id } = req.params;
+      await db(`DELETE FROM users WHERE id="${id}";`);
+      res.send({
+        msg: `User with id '${id}' successfully deleted from database`,
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).send(err);
+    }
   }
-});
+);
 
-
-// disallow dupicate user names in the post method using a guard function, 
-// if household or primary account column is enabled later that function can 
+// disallow dupicate user names in the post method using a guard function,
+// if household or primary account column is enabled later that function can
 // change to make sure there are no duplicates within the household instead of within the database,
 // and the get user by userName function can become get by userName become by userName-household
 
 module.exports = router;
-
-
