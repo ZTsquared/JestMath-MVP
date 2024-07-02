110b3a5b6bbe10905999efc1b2c3538b20a3a2ff
ZTsquared <Zia.Tyebjee@Gmail.com>
2024-07-03 01:00:24 +0200

    still trying to resolve build error

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
   // Specify a custom directory for Next.js to look for pages
   pageExtensions: ["jsx"], // Include any other extensions you are using
   webpack(config, options) {
-    config.resolve.alias["~"] = path.resolve(__dirname, "client/pages"); // Adjust the path as needed
+    config.resolve.alias["~"] = path.resolve(__dirname, "client/pages/src"); // Adjust the path as needed
     return config;
   },
 };
